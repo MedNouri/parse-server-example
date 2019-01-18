7217b600a470cef63381789d90b586e63ae0b9b1
mehdi <mehdisouid94@gmail.com>
2019-01-18 12:10:30 +0100

    make it better

--- cloud/main.js
+++ cloud/main.js
@@ -1,31 +1,44 @@
- 
-
-Parse.Cloud.define('sendPush', function (request, response) {
-	var params = request.params;
-	var customData = params.customData;
+ Parse.Cloud.define('sendPush', function (request, response) {
+ 	var params = request.params;
+ 	var customData = params.customData;
  	var message = params.message;
- 
-	if (!customData) {
-		response.error("Missing customData!")
-	}
 
-	//var sender = JSON.parse(customData).sender;
-	var query = new Parse.Query(Parse.Installation);
-	query.equalTo("installationId", customData);
+ 	if (!customData) {
+ 		response.error("Missing customData!")
+ 	}
 
-	Parse.Push.send({
-		where: query,
-		// Parse.Push requires a dictionary, not a string.
-		data: { "alert": message },
-	}, {
-			success: function () {
-				console.log("#### PUSH OK");
-			}, error: function (error) {
-				console.log("#### PUSH ERROR" + error.message);
-			}, useMasterKey: true
-		});
+ 	//var sender = JSON.parse(customData).sender;
+ 	var query = new Parse.Query(Parse.Installation);
+ 	query.equalTo("installationId", customData);
 
-	//	response.json('success');
-});
+ 	Parse.Push.send({
+ 		where: query,
+ 		// Parse.Push requires a dictionary, not a string.
+ 		data: {
+ 			"alert": message
+ 		},
+ 	}, {
+ 		success: function () {
+ 			console.log("#### PUSH OK");
+ 		},
+ 		error: function (error) {
+ 			console.log("#### PUSH ERROR" + error.message);
+ 		},
+ 		useMasterKey: true
+ 	});
 
- 
+ 	//	response.json('success');
+ });
+ Parse.Cloud.beforeSave(Parse.User, (request) => {
+ 	if (!validateEmail(request.object.get("email"))) {
+ 		throw "email invalide";
+ 	}
+
+
+ });
+
+
+ function validateEmail(email) {
+ 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ 	return re.test(String(email).toLowerCase());
+ }
