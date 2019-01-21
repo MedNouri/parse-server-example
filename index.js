35548f1c410fcbf5833243048ee7a9702f66e5b2
mehdi <mehdisouid94@gmail.com>
2019-01-21 10:15:40 +0100

    maj

--- index.js
+++ index.js
@@ -60,43 +60,9 @@ var api = new ParseServer({
       // Your API key from mailgun.com
       apiKey: 'af7fb646350c3486c351e26664648ab6-3939b93a-fd198105',
     }
-  },
-  push: {
-    android: {
-      apiKey: 'AIzaSyDIRGhEXKP00RKrS0PiqplkmTVOlL5z3Y8'
-    },
-    ios: {
-      pfx: '', // The filename of private key and certificate in PFX or PKCS12 format from disk  
-      passphrase: '', // optional password to your p12
-      cert: '', // If not using the .p12 format, the path to the certificate PEM to load from disk
-      key: '', // If not using the .p12 format, the path to the private key PEM to load from disk
-      bundleId: '', // The bundle identifier associated with your app
-      production: false // Specifies which APNS environment to connect to: Production (if true) or Sandbox
-    }
   }
-
-  // account lockout policy setting (OPTIONAL) - defaults to undefined
-  // if the account lockout policy is set and there are more than `threshold` number of failed login attempts then the `login` api call returns error code `Parse.Error.OBJECT_NOT_FOUND` with error message `Your account is locked due to multiple failed login attempts. Please try again after <duration> minute(s)`. After `duration` minutes of no login attempts, the application will allow the user to try login again.
-  // accountLockout: {
-  //   duration: 5, // duration policy setting determines the number of minutes that a locked-out account remains locked out before automatically becoming unlocked. Set it to a value greater than 0 and less than 100000.
-  //   threshold: 3, // threshold policy setting determines the number of failed sign-in attempts that will cause a user account to be locked. Set it to an integer value greater than 0 and less than 1000.
-  // }
-
-  /* ,
-  // optional settings to enforce password policies
-  passwordPolicy: {
-    // Two optional settings to enforce strong passwords. Either one or both can be specified. 
-    // If both are specified, both checks must pass to accept the password
-    // 1. a RegExp object or a regex string representing the pattern to enforce 
-    validatorPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, // enforce password with at least 8 char with at least 1 lower case, 1 upper case and 1 digit
-    // 2. a callback function to be invoked to validate the password  
-    validatorCallback: (password) => { return validatePassword(password) }, 
-    doNotAllowUsername: true, // optional setting to disallow username in passwords
-    maxPasswordAge: 90, // optional setting in days for password expiry. Login fails if user does not reset the password within this period after signup/last reset. 
-    maxPasswordHistory: 5, // optional setting to prevent reuse of previous n passwords. Maximum value that can be specified is 20. Not specifying it or specifying 0 will not enforce history.
-    //optional setting to set a validity duration for password reset links (in seconds)
-    resetTokenValidityDuration: 24*60*60, // expire after 24 hours
-  } */
+ 
+ 
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
