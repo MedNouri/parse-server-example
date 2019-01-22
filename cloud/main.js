7916216cab4717b811531d320bec7ffe565c2bef
mehdi <mehdisouid94@gmail.com>
2019-01-22 15:12:43 +0100

    maj

--- cloud/main.js
+++ cloud/main.js
@@ -58,30 +58,30 @@ Parse.Cloud.beforeSave(Parse.User, (request) => {
 Parse.Cloud.beforeSave("Company", (request) => {
 	var companyType = ["Installateur / Fabriquant", "exposant"]
 
-	/* 	if (request.object.get("companyForm") == null) {
-			throw "Can't Creat company if companyForm is empty .";
-		}
-		if (request.object.get("companyNumSiret") == null) {
-			throw "Can't Creat company if companyNumSiret is empty .";
-		} */
+	if (request.object.get("companyForm") == null) {
+		throw "Can't Creat company if companyForm is empty .";
+	}
+	if (request.object.get("companyNumSiret") == null) {
+		throw "Can't Creat company if companyNumSiret is empty .";
+	}
 	if (request.object.get("companyName") == null) {
 		throw "Can't Creat company if companyName is empty .";
 	}
 
-	/* 	if (request.object.get("companyName") == null) {
-			throw "Can't Creat company if companyName is empty .";
-		}
-		if (request.object.get("companyCity") == null) {
-			throw "Can't Creat company if companyCity is empty .";
-		}
-		if (request.object.get("companyLogo") == null) {
-			throw "Can't Creat company if companyLogo is empty .";
-		}
-		if (request.object.gMet("companyAddress") == null) {
-			throw "Can't Creat company if companyAddress is empty .";
-		}
+	if (request.object.get("companyName") == null) {
+		throw "Can't Creat company if companyName is empty .";
+	}
+	if (request.object.get("companyCity") == null) {
+		throw "Can't Creat company if companyCity is empty .";
+	}
+	if (request.object.get("companyLogo") == null) {
+		throw "Can't Creat company if companyLogo is empty .";
+	}
+	if (request.object.gMet("companyAddress") == null) {
+		throw "Can't Creat company if companyAddress is empty .";
+	}
 
-		*/
+
 	if (request.object.get("companytype") == null) {
 		throw "Can't Creat company if companytype is empty .";
 	} else {
